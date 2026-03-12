7c8a1b402533e76fbc41caa41d5d7220aecd9cf0
Nurlan949 <nurlanali246@gmail.com>
2026-03-12 15:15:03 +0400

    entity @Table update

--- src/main/java/abb/tech/auth_service/model/Role.java
+++ src/main/java/abb/tech/auth_service/model/Role.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
--- src/main/java/abb/tech/auth_service/model/User.java
+++ src/main/java/abb/tech/auth_service/model/User.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Getter
 @Setter
 @AllArgsConstructor
